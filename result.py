85435dee88e205f10d90c837c2f25801a3a2bc5b
ruslan <vergilijus@ya.ru>
2018-11-01 11:35:50 +0300

    result: wrapper и визуализация

--- result.py
+++ result.py
@@ -1,5 +1,20 @@
 import cv2 as cv
 import numpy as np
+import pandas as pd
+
+
+class ResultWrapper:
+    def __init__(self, results_dict) -> None:
+        self.results = results_dict
+
+    def get_df(self, fields=('id', 'score', 'a', 'b', 'area')):
+        table = []
+        params_dict = get_params_from_results(self.results)
+        for name, params_list in params_dict.items():
+            for p in params_list:
+                row = (name, *tuple(p[k] for k in fields))
+                table.append(row)
+        return pd.DataFrame(table, columns=['name', *fields])
 
 
 def get_params_from_result(result):
@@ -8,9 +23,11 @@ def get_params_from_result(result):
         mask = result['masks'][:, :, i]
         _, contours, _ = cv.findContours(mask.astype(np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_TC89_KCOS)
         cnt = contours[0]
+        if len(cnt) < 5:
+            continue
         ellipse = cv.fitEllipse(cnt)
-        s1 = ellipse[1][0] / 2,
-        s2 = ellipse[1][1] / 2,
+        s1 = ellipse[1][0] / 2
+        s2 = ellipse[1][1] / 2
         params = {
             'id': i,
             'cnt': cnt,
@@ -33,16 +50,19 @@ def get_params_from_results(results_dict):
 
 
 def visualize_params(img, params_list):
+    k = max(img.shape) / 1024
+    t = round(k)
+    scale = k * 1.2
     for params in params_list:
         # contours
-        cv.ellipse(img, params['ellipse'], (255, 0, 0), 1, cv.LINE_AA)
-        cv.drawContours(img, [params['cnt']], -1, (0, 255, 0), 1, cv.LINE_AA)
+        cv.ellipse(img, params['ellipse'], (0, 0, 255), t, cv.LINE_AA)
+        cv.drawContours(img, [params['cnt']], -1, (0, 255, 0), t, cv.LINE_AA)
         # text
         y1, x1, y2, x2 = params['box']
         text = '{id} {score:.2f}'.format(**params)
         font = cv.FONT_HERSHEY_PLAIN
-        cv.putText(img, text, (x1 + 1, y1 + 1), font, 1.2, (0, 0, 0), 2, cv.LINE_AA)
-        cv.putText(img, text, (x1, y1), font, 1.2, (255, 255, 255), 1, cv.LINE_AA)
+        cv.putText(img, text, (x1 + 1, y1 + 1), font, scale, (0, 0, 0), 2 * t, cv.LINE_AA)
+        cv.putText(img, text, (x1, y1), font, scale, (255, 255, 255), t, cv.LINE_AA)
 
 
 def visualize_all_params(images_dict, params_dict):
@@ -52,5 +72,5 @@ def visualize_all_params(images_dict, params_dict):
 
 
 def get_params_and_visualize(images_dict, results_dict):
-    params_dicts = get_params_from_results(results_dict)
-    visualize_all_params(images_dict, params_dicts)
+    params_dict = get_params_from_results(results_dict)
+    visualize_all_params(images_dict, params_dict)
