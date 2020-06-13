c65a96a9462d2221331694459ae5cea79567fde1
ruslan <vergilijus@ya.ru>
2020-06-13 13:49:58 +0300

    coco evaluation

--- detector.py
+++ detector.py
@@ -27,6 +27,10 @@ from imgaug import augmenters as iaa
 import cv2 as cv
 import time
 
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from pycocotools import mask as maskUtils
+
 # Root directory of the project
 ROOT_DIR = os.path.abspath("./")
 
@@ -72,10 +76,8 @@ class VesicleConfig(Config):
     # resnet 50 or resnet101
     BACKBONE = "resnet50"
 
-    IMAGE_CHANNEL_COUNT = 1
-    MEAN_PIXEL = np.array([148.4])
-
     LEARNING_RATE = 0.001
+    IMAGE_RESIZE_MODE = "none"
 
 
 class VesicleGrayConfig(VesicleConfig):
@@ -87,6 +89,7 @@ class VesicleInferenceConfig(VesicleConfig):
     # Set batch size to 1 to run one image at a time
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
+    IMAGE_RESIZE_MODE = "none"
 
 
 ############################################################
@@ -94,8 +97,8 @@ class VesicleInferenceConfig(VesicleConfig):
 ############################################################
 
 class VesicleDataset(utils.Dataset):
-    def load_image(self, image_id):
-        return cv.imread(self.image_info[image_id]['path'], 0)[..., np.newaxis]
+    # def load_image(self, image_id):
+    #     return cv.imread(self.image_info[image_id]['path'], 0)[..., np.newaxis]
 
     def load_vesicle(self, dataset_dir, subset):
         """Load a subset of the Vesicles dataset.
@@ -161,6 +164,55 @@ class VesicleDataset(utils.Dataset):
         # one class ID only, we return an array of 1s
         return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
 
+    def load_coco(self, dataset_dir, subset, class_ids=None):
+        """Load a subset of the COCO dataset.
+        dataset_dir: The root directory of the COCO dataset.
+        subset: What to load (train, val, minival, valminusminival)
+        year: What dataset year to load (2014, 2017) as a string, not an integer
+        class_ids: If provided, only loads images that have the given classes.
+        class_map: TODO: Not implemented yet. Supports maping classes from
+            different datasets to the same class ID.
+        return_coco: If True, returns the COCO object.
+        auto_download: Automatically download and unzip MS-COCO images and annotations
+        """
+
+        coco = COCO("{}/{}/instances_annotations.json".format(dataset_dir, subset))
+        if subset == "minival" or subset == "valminusminival":
+            subset = "val"
+        image_dir = "{}/{}".format(dataset_dir, subset)
+
+        # Load all classes or a subset?
+        if not class_ids:
+            # All classes
+            class_ids = sorted(coco.getCatIds())
+
+        # All images or a subset?
+        if class_ids:
+            image_ids = []
+            for id in class_ids:
+                image_ids.extend(list(coco.getImgIds(catIds=[id])))
+            # Remove duplicates
+            image_ids = list(set(image_ids))
+        else:
+            # All images
+            image_ids = list(coco.imgs.keys())
+
+        # Add classes
+        for i in class_ids:
+            self.add_class("coco", i, coco.loadCats(i)[0]["name"])
+
+        # Add images
+        for i in image_ids:
+            self.add_image(
+                "coco", image_id=i,
+                path=os.path.join(image_dir, coco.imgs[i]['file_name']),
+                width=coco.imgs[i]["width"],
+                height=coco.imgs[i]["height"],
+                annotations=coco.loadAnns(coco.getAnnIds(
+                    imgIds=[i], catIds=class_ids, iscrowd=None)))
+
+        return coco
+
     def image_reference(self, image_id):
         """Return the path of the image."""
         info = self.image_info[image_id]
@@ -199,6 +251,85 @@ def train(model, epochs=EPOCHS):
                 epochs=epochs,
                 augmentation=augmentation,
                 layers='all')
+
+
+def build_coco_results(dataset, image_ids, rois, class_ids, scores, masks):
+    """Arrange resutls to match COCO specs in http://cocodataset.org/#format
+    """
+    # If no results, return an empty list
+    if rois is None:
+        return []
+
+    results = []
+    for image_id in image_ids:
+        # Loop through detections
+        for i in range(rois.shape[0]):
+            class_id = class_ids[i]
+            score = scores[i]
+            bbox = np.around(rois[i], 1)
+            mask = masks[:, :, i]
+
+            result = {
+                "image_id": image_id,
+                "category_id": dataset.get_source_class_id(class_id, "coco"),
+                "bbox": [bbox[1], bbox[0], bbox[3] - bbox[1], bbox[2] - bbox[0]],
+                "score": score,
+                "segmentation": maskUtils.encode(np.asfortranarray(mask))
+            }
+            results.append(result)
+    return results
+
+
+def evaluate_coco(model, dataset, coco, eval_type="segm", limit=0, image_ids=None):
+    """Runs official COCO evaluation.
+    dataset: A Dataset object with valiadtion data
+    eval_type: "bbox" or "segm" for bounding box or segmentation evaluation
+    limit: if not 0, it's the number of images to use for evaluation
+    """
+    # Pick COCO images from the dataset
+    image_ids = image_ids or dataset.image_ids
+
+    # Limit to a subset
+    if limit:
+        image_ids = image_ids[:limit]
+
+    # Get corresponding COCO image IDs.
+    coco_image_ids = [dataset.image_info[id]["id"] for id in image_ids]
+    print(coco_image_ids)
+    t_prediction = 0
+    t_start = time.time()
+
+    results = []
+    for i, image_id in enumerate(image_ids):
+        # Load image
+        image = dataset.load_image(image_id)
+
+        # Run detection
+        t = time.time()
+        r = model.detect([image], verbose=0)[0]
+        t_prediction += (time.time() - t)
+
+        # Convert results to COCO format
+        # Cast masks to uint8 because COCO tools errors out on bool
+        image_results = build_coco_results(dataset, coco_image_ids[i:i + 1],
+                                           r["rois"], r["class_ids"],
+                                           r["scores"],
+                                           r["masks"].astype(np.uint8))
+        results.extend(image_results)
+
+    # Load results. This modifies results with additional attributes.
+    coco_results = coco.loadRes(results)
+
+    # Evaluate
+    cocoEval = COCOeval(coco, coco_results, eval_type)
+    cocoEval.params.imgIds = coco_image_ids
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    cocoEval.summarize()
+
+    print("Prediction time: {}. Average {}/image".format(
+        t_prediction, t_prediction / len(image_ids)))
+    print("Total time: ", time.time() - t_start)
 
 
 ############################################################
@@ -291,6 +422,13 @@ if __name__ == '__main__':
     # Train or evaluate
     if args.command == "train":
         train(model, args.epochs)
+    elif args.command == "evaluate":
+        # Validation dataset
+        dataset_val = VesicleDataset()
+        coco = dataset_val.load_coco(args.dataset, 'val')
+        dataset_val.prepare()
+        print("Running COCO evaluation on images.")
+        evaluate_coco(model, dataset_val, coco, "segm", limit=3)
     else:
         print("'{}' is not recognized. "
               "Use 'train'".format(args.command))
